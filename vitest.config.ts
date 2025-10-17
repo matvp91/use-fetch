af4a02d147bec3ab9fc85cd04e272c21d8e0f146
Matthias Van Parijs <matvp91@gmail.com>
2025-10-17 18:31:58 +0200

    chore: Only include src

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     coverage: {
       reporter: ["text", "json-summary", "json"],
       reportOnFailure: true,
-      exclude: ["./playground"],
+      include: ["./src"],
     },
   },
 });
